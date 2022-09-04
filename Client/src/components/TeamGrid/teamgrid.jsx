506fe39eaa0358150eba6a27e63bb943c88b5bf2
Mark Miller <markmiller7858@gmail.com>
2022-09-04 09:55:13 -0400

    search bar added to grid

--- Client/src/components/TeamGrid/teamgrid.jsx
+++ Client/src/components/TeamGrid/teamgrid.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './teamgrid.css';
 import TeamCard from "../Teamcard/teamcard";
 
@@ -8,11 +8,60 @@ import { useStoreContext } from "../../context/globalState";
 const TeamGrid = () =>{
     const [state, dispatch] = useStoreContext();
     const {teams} = state;
+
+    const [displayedTeams, setDisplayedTeams] = useState(teams);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    function handleChange(e){
+        setSearchTerm(e.target.value);
+        console.log(searchTerm);
+    };
+
+    function handleSubmit(e){
+        e.preventDefault();
+        teamSearch(teams, searchTerm);
+    };
+
+    const teamSearch = (allteams, keyword) =>{
+        const searchTerm = keyword.toLowerCase();
+        // const schoolSearch = allteams.school
+        //                     .split(' ')
+        //                     .join('')
+        //                     .toLowerCase();
+        // const mascotSearch = allteams.mascot
+        //                     .split(' ')
+        //                     .join('')
+        //                     .toLowerCase();
+
+        const teamsToShow = allteams.filter(team => {
+            return team.school.split(' ').join('').toLowerCase().match(new RegExp(searchTerm, 'g')) ||
+            team.mascot.split(' ').join('').toLowerCase().match(new RegExp(searchTerm, 'g'));
+        });
+
+        console.log(teamsToShow);
+
+        if(teamsToShow !==[]){
+            setDisplayedTeams(teamsToShow)
+        };
+    }
     
     return(
         <div className="teamsGrid container">
             <div className="row">
-                {teams.map(({id, school, mascot, color, altColor, logos}) =>(
+                <form className="col-12 searchBar" onSubmit={handleSubmit}>
+                    <label htmlFor="team-search">
+                        <span className="visually-hidden">Search for teams</span>
+                    </label>
+                    <input
+                        type='text'
+                        id='team-search'
+                        placeholder="Team Name"
+                        name="team-search"
+                        onChange={handleChange}
+                    />
+                    <button type='submit'>Search</button>
+                </form>
+                {displayedTeams.map(({id, school, mascot, color, altColor, logos}) =>(
                     <TeamCard 
                         key={id} 
                         school={school} 
--- Client/src/components/Teamcard/teamcard.jsx
+++ Client/src/components/Teamcard/teamcard.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './teamcard.css'
 
 import fontColors from "../../utils/fontColors";
 
 const TeamCard = ({id, school, mascot, primaryColor, secondaryColor, logo}) => {
 
+    const [isHovering, setIsHovering] = useState(-1);
+
     const teamColors = {
         color: fontColors(school) || primaryColor,
-        backgroundColor: secondaryColor
+        backgroundColor: secondaryColor === null? 'white' : secondaryColor
     };
 
     return (
-        <div className="teamCard col-2"
+        <div className={`teamCard col-2 ${isHovering === id? 'cardHover':''}`}
+            key={id}
+            onMouseEnter={() => setIsHovering(id)} 
+            onMouseLeave={() => setIsHovering(-1)}
             style={teamColors}>
-            <p>{school}</p>
-            <img alt= {school} src={logo}/>
-            <p>{mascot}</p>
+                <p>{school}</p>
+                <img alt= {school} src={logo}/>
+                <p>{mascot}</p>
         </div>
     )
 };
